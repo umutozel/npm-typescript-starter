9a60dfe35fc5c2da210496250a30f9e507acc0bf
umutozel <umutozel@gmail.com>
2018-07-15 22:39:55 +0300

    Fix dummy test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,7 +4,7 @@ import { test } from '../lib/index';
 
 describe('index', function () {
     describe('test', function () {
-        it('should return 42', function (done) {
+        it('should return 42', function () {
             const result = test();
 
             expect(result).to.equal(42);
